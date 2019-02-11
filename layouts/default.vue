0e75cc09db03dbb35f9f221ea6f285493db7d918
Максим Кутьков <maksimkutkov@MBP-Maksim.Dlink>
2019-02-12 01:26:28 +0300

    bild inner page

--- layouts/default.vue
+++ layouts/default.vue
@@ -33,6 +33,29 @@ export default {
       color: #02adc6;
       font-weight: bold;
   }
+    .h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
+        margin-bottom: .5rem;
+        line-height: 1.5;
+        color: inherit;
+    }
+    .h1, h1 {
+        font-size: 2.5rem;
+    }
+    .h2, h2 {
+        font-size: 2rem;
+    }
+    .h3, h3 {
+        font-size: 1.75rem;
+    }
+    .h4, h4 {
+        font-size: 1.5rem;
+    }
+    .h5, h5 {
+        font-size: 1.25rem;
+    }
+    .h6, h6 {
+        font-size: 1rem;
+    }
   h4{
       line-height: 25px;
   }
@@ -48,5 +71,13 @@ export default {
   .dr-quote{
       background: url('~assets/custom-img/parallax/tibet-sky.jpg') no-repeat fixed;
   }
+  /* inner page css */
+    .header{
+        box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.32);
+    }
+    .departments .depart-sec{
+        padding: 50px 0;
+    }
+    /* end inner page css */
 </style>
 
